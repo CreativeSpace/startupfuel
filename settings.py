10a8f15efff097f443d838bcd8759f02cf22c548
Jason Peddle <jwpeddle@gmail.com>
2011-06-04 11:04:06 -0400

    fixed stray comma

--- settings.py
+++ settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static'), # Or path to database file if using sqlite3.
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static') # Or path to database file if using sqlite3.
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
