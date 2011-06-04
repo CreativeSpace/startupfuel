c4e1cc918049106164a47fbd1dde042e4bfa570d
Jason Peddle <jwpeddle@gmail.com>
2011-06-04 15:35:28 -0400

    dynamic donations

--- startups/models.py
+++ startups/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 
 class Startup(models.Model):
     name = models.CharField(max_length=100)
@@ -9,9 +10,13 @@ class Startup(models.Model):
     twitter = models.CharField(max_length=50, blank=True)
     video = models.CharField(max_length=50, blank=True)
     paypal_email = models.EmailField(blank=True)
+    goal = models.IntegerField(null=True, blank=True)
 
     def __unicode__(self):
         return self.name
+
+    def amount_raised(self):
+        return Donation.objects.filter(startup=self).aggregate(amount_raised=Sum('amount'))['amount_raised']
 
 class Tier(models.Model):
     startup = models.ForeignKey(Startup)
@@ -19,7 +24,7 @@ class Tier(models.Model):
     incentive = models.CharField(max_length=150)
 
     def __unicode__(self):
-        return str(self.value)
+        return str(self.amount)
 
 class Donation(models.Model):
     startup = models.ForeignKey(Startup)
