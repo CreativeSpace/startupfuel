29a43d59f4a9edb844fec3fff5f988b80dc7c67e
Aron Jones <defrex0@gmail.com>
2011-06-04 21:30:46 -0400

    swap like button

--- project_static/js/the.js
+++ project_static/js/the.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
   });
 
   //lazy-load twitter/facebook
-  $('#facebook').html('<iframe src="http://www.facebook.com/plugins/like.php?app_id=171186796274800&amp;href&amp;send=false&amp;layout=button_count&amp;width=150&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>');
+  $('#facebook').html('<iframe src="http://www.facebook.com/plugins/like.php?app_id=206457229389922&amp;href=http%3A%2F%2Fstartupfuel.ca%2Fhipsell%2F&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>');
   $('#twitter').append($('<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>'));
 });
 
