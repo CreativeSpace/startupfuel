66ba92a450d3e212e00657101b8da45fdf4cff8b
Aron Jones <defrex0@gmail.com>
2011-06-05 14:20:55 -0400

    gar bar working

--- startups/utils.py
+++ startups/utils.py
@@ -4,4 +4,5 @@ from models import Donation
 
 def get_percent_done():
     total = Donation.objects.aggregate(amount_raised=Sum('amount'))['amount_raised'] or 0
-    return (100.0 - total / 500.0)
+    print total
+    return (100.0 - (total / 500.0)*100)
